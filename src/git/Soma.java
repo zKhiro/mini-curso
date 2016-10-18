4a63e9611255d54ebd4f8b084f1c03113e49122a
Leandro Matias <leandro.khiro@gmail.com>
2016-10-18 20:42:13 -0200

    Commit inicial

---
+++ src/git/Soma.java
@@ -0,0 +1,17 @@
+package git;
+
+public class Soma {
+	int x;
+	int y;
+	int result;
+	
+	public Soma(int x, int y){
+		this.x=x;
+		this.y=y;
+		result = x+y;
+	}
+	
+/*	public int getResult(){
+		return result;
+	}*/
+}
